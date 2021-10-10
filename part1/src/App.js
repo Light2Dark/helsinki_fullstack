1de43531881c0a0f149fb88ddfb29378ebe9a2c8
Shahmir Varqha <Sham9871@gmail.com>
2021-10-10 17:23:19 +0800

    part 3 lecture on simple states in react

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,25 +1,43 @@
-import React from 'react'
+import React, {useState} from 'react' // importing the useState function
 
-const App = () => {
-  
-  const name = "Peter"
-  const age = 10
+const App = (props) => {
+  const [counter, setCounter] = useState(0) // adds state to the component and renders it initialized with value of 0. returns an array with 2 items which are assigned to variables counter..
+
+  // setTimeout(
+  //   () => setCounter(counter + 1), // react will rerender this component
+  //   1000
+  // )
+
+  // when the component is re-executed, useState is called again but this time returns the value of 1. setTimeout function that occurs again to increment counter value and re-render component
+
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter-1)
+  const setToZero = () => setCounter(0)
 
   return (
     <>
-      <h1>Greetings</h1>
-      <Hello name = "Blear" age = {26 + 10} />
-      <Hello name = {name} age = {age} />
+      <Display counter = {counter} />
+      <Button onClick = {increaseByOne} text = "plus" />
+      <Button onClick = {decreaseByOne} text = "minus" />
+      <Button onClick = {setToZero} text = "zero" />
     </>
   )
 }
 // same as const App = () => (...)
 
-const Hello = (props) => {
+const Button = ({onClick, text}) => <button onClick = {onClick}>{text}</button>
+
+const Display = ({counter}) => <div>{counter}</div>
+
+const Hello = ({name, age}) => {
+
+  const bornYear = () => new Date().getFullYear() - age
+
   return (
-    <>
-      <p>Hello {props.name}. You are {props.age} years old!</p>
-    </>
+    <div>
+      <p>Hello {name}. You are {age} years old!</p>
+      <p>So, you were probably born in {bornYear()}</p>
+    </div>
   )
 }
 
--- part1/src/index.js
+++ part1/src/index.js
@@ -1,7 +1,16 @@
 import ReactDOM from 'react-dom';
 import App from './App';
 
+let counter = 1
+
 ReactDOM.render(
   <App />,
   document.getElementById("root")
 )
+
+const badWay = () => {
+  setInterval(() => {
+    // refresh()
+    counter++
+  }, 1000)
+}
