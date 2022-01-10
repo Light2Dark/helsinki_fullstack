909aae8f57b9cda9686577c5d0051acf94489098
Shahmir Varqha <Sham9871@gmail.com>
2022-01-11 03:16:13 +0800

    theory part of part 3

---
+++ part3/web-server/index.js
@@ -0,0 +1,91 @@
+const express = require("express")
+const app = express()
+
+app.use(express.json()) // json-parser
+
+let notes = [
+    {
+      id: 1,
+      content: "HTML is easy",
+      date: "2019-05-30T17:30:31.098Z",
+      important: true
+    },
+    {
+      id: 2,
+      content: "Browser can execute only Javascript",
+      date: "2019-05-30T18:39:34.091Z",
+      important: false
+    },
+    {
+      id: 3,
+      content: "GET and POST are the most important methods of HTTP protocol",
+      date: "2019-05-30T19:20:14.298Z",
+      important: true
+    }
+]
+
+app.get("/", (request, response) => {
+    response.send("<h1>Hello Worleder</h1>")
+})
+
+app.get("/api/notes", (request, response) => {
+    response.json(notes)
+}) // handles HTTP GET requests to the notes path of the application
+
+app.get("/api/notes/:id", (request, response) => {
+    const id = Number(request.params.id)
+    const note = notes.find(note => {
+        // console.log(note.id, typeof note.id, id, typeof id, note.id === id) check terminal for console.logs
+        return note.id === id
+    })
+
+    if (note) {
+        response.json(note) // undefined is a falsy value
+    } else {
+        response.status(404).end() 
+        // response.statusMessage=".."
+        // res.status(400).end()
+    }
+
+})
+
+app.delete("/api/notes/:id", (req, res) => {
+    const id = Number(req.params.id)
+    notes = notes.filter(note => note.id !== id)
+
+    response.status(204).end()
+})
+
+const generateId = () => {
+    const maxId = notes.length > 0 
+        ? Math.max(...notes.map(note => note.id)) // array is transformed to individual numbers through ...
+        : 0
+
+    return maxId + 1
+}
+
+app.post("/api/notes", (request, response) => {
+    const body = request.body // body part recognized by json parser
+
+    if(!body.content) {
+        return response.status(400).json({
+            error: "Content missing"
+        }) // return is rly important so that the broken note is not stored, cuz rest of post request will still execute
+    }
+
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        date: new Date(),
+        id: generateId()
+    }
+
+    notes = notes.concat(note)
+
+    response.json(note)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`)
+})
